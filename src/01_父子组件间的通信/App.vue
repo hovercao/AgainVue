2b27b4f12bf4a4cafa8485c3309d59e49b6b1af6
hovercao <43169035+hovercao@users.noreply.github.com>
2022-09-04 21:41:16 +0800

    父子组件通信

---
+++ src/01_父子组件间的通信/App.vue
@@ -0,0 +1,29 @@
+<template>
+  {{ count }}
+  <AddCount @addCounter="addNumber"></AddCount>
+</template>
+
+<script>
+import AddCount from "@/01_父子组件间的通信/AddCount";
+
+export default {
+  name: "App",
+  components: {
+    AddCount
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addNumber(count) {
+      this.count += count
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
+import App from "@/01_父子组件间的通信/App";
 createApp(App).mount('#app')
