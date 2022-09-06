05093e1e0cc632e888a5f15bb87ccc93ee0d32e7
hovercao <43169035+hovercao@users.noreply.github.com>
2022-09-06 21:37:18 +0800

    feat:组件中v-model的使用

--- src/09_组件的v-model/App.vue
+++ src/09_组件的v-model/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <!--  <Counter v-model:model-value="value" @update:modelValue="value=$event"></Counter>-->
+  <!--  等价于-->
+  <Counter v-model="value"></Counter>
 
 </template>
 
 <script>
+import Counter from "@/09_组件的v-model/Counter";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {Counter},
+  data() {
+    return {
+      value: 1
+    }
+  }
 }
 </script>
 
---
+++ src/09_组件的v-model/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{ modelValue }}</span>
+  <button @click="handleClick">change</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  emits: ['update:modelValue'],
+  props: {
+    modelValue: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('update:modelValue', 100)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
