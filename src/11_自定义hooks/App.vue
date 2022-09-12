c2056200772d56fbf1cdda0fbe6c4a0cea750db6
hovercao <17388766232@163.com>
2022-09-12 09:51:32 +0800

    feat:自定义hooks

---
+++ src/11_自定义hooks/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ scrollPosition.y }}</div>
+  <div>{{ scrollPosition.x }}</div>
+  <div class="App">App</div>
+
+</template>
+<script setup>
+import useScrollPosition from "@/11_自定义hooks/hooks/useScrollPosition";
+
+const {scrollPosition} = useScrollPosition()
+
+</script>
+<style scoped>
+.App {
+  height: 3000px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import {createApp} from 'vue'
 // import App from "@/08_keep-alive/App";
 // import App from "@/09_组件的v-model/App";
 // import App from '@/10_vue3的内置函数/App'
+//import App from '@/11_自定义hooks/App'
+// import App from '@/10_vue3的内置函数/App'
 import App from '@/11_vue路由/App'
 
 import router from "@/Router";
