f9aef76461eb65359cedad3d423bdc96f3c55986
hovercao <17388766232@163.com>
2022-09-12 09:51:04 +0800

    feat:自定义hooks

---
+++ src/11_自定义hooks/hooks/useScrollPosition.js
@@ -0,0 +1,16 @@
+import {reactive} from "vue";
+
+export default function () {
+  const scrollPosition = reactive({
+    x: 0, y: 0
+  })
+  
+  document.addEventListener('scroll', () => {
+    scrollPosition.x = window.scrollX
+    scrollPosition.y = window.scrollY
+  })
+  
+  return {
+    scrollPosition
+  }
+}
