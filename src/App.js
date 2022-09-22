bac992df5c640aeeb992e7dac102b207b4e718ac
hovercao <17388766232@163.com>
2022-09-22 08:25:39 +0800

    feat:react初始化

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: hovercao 17388766232@163.com
+ * @Date: 2022-09-22 07:52:03
+ * @LastEditors: hovercao 17388766232@163.com
+ * @LastEditTime: 2022-09-22 07:54:53
+ * @FilePath: \AgainVue\src\App.js
+ */
+import { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <>app</>;
+  }
+}
